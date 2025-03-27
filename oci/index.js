d07a48a13d9677331ae4d156aa2feaf944de7268
namickey <teethdai@gmail.com>
2025-03-27 21:59:57 +0900

    add

--- oci/index.js
+++ oci/index.js
@@ -38,6 +38,37 @@ app.get('/aa', (req, res) => {
     res.send('push ok!');
 });
 
+app.get('/wakapushsub', (req, res) => {
+
+    // VAPIDキーの読み込み
+    const vapidKeys = JSON.parse(fs.readFileSync('./vapid-waka.json.key', 'utf8'));
+    // Push Subscriptionの読み込み
+    const subscription = JSON.parse(fs.readFileSync('./push-subscription-waka.json.key', 'utf8'));
+
+    // VAPID設定
+    webPush.setVapidDetails(
+        vapidKeys['mailto'],
+        vapidKeys['public-key'],
+        vapidKeys['private-key']
+    );
+
+    // 通知内容
+    const notification = {
+        title: 'わかメール',
+        body: 'メールが届きました'
+    };
+
+    // 通知送信
+    webPush.sendNotification(subscription, JSON.stringify(notification))
+        .then(() => {
+            console.log('通知を送信しました');
+        })
+        .catch(err => {
+            console.error('通知の送信に失敗しました:', err);
+        });
+    res.send('push ok!');
+});
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
